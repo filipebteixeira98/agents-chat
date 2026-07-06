160399dd2e2b75915db5079a544d825b9caef3ed
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-06 01:43:15 -0300

    feat(rooms): add list rooms endpoint

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ import {
 
 import { env } from './env.ts'
 
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
@@ -18,5 +20,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => 'OK')
+
+app.register(getRoomsRoute)
 
 app.listen({ port: env.PORT })
